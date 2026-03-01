4101228ebf0430c19d0738521a202b1942654ff8
alenaistomina47-creator <alenaistomina47@gmail.com>
2026-03-01 15:50:34 +0300

    Add files via upload

--- js/store.js
+++ js/store.js
@@ -23,7 +23,7 @@ document.addEventListener('alpine:init', () => {
         isLoading: true, // Состояние загрузки изображений
 
         // WEBHOOK URL
-        webhookUrl: 'https://acknowledged-chemistry-memory-soviet.trycloudflare.com/webhook/save-cart',
+        webhookUrl: 'https://kuklin-n8n-bot.ru/webhook/save-cart',
 
 
         // Инициализация
